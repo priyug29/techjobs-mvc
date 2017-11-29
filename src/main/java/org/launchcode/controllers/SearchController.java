da691bdf35f96123ec2ece1f14be669d077e6415
Priyanka Gupta <priyug29@gmail.com>
2017-11-29 10:05:13 -0600

    Added handler for search request and display results with view as search

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -4,6 +4,7 @@ import org.launchcode.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
@@ -19,9 +20,34 @@ public class SearchController {
     @RequestMapping(value = "")
     public String search(Model model) {
         model.addAttribute("columns", ListController.columnChoices);
+
         return "search";
     }
 
     // TODO #1 - Create handler to process search request and display results
+
+
+    @RequestMapping(value = "results", method = RequestMethod.GET)
+    public String searchResults(Model model, @RequestParam String searchType,
+                                @RequestParam String searchTerm) {
+
+        model.addAttribute("columns", ListController.columnChoices);
+
+        ArrayList<HashMap<String, String>> jobs;
+
+        if(searchType.equals("all")){
+            jobs = JobData.findByValue(searchTerm);
+            model.addAttribute("title", "All Jobs");
+        } else {
+            jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+            model.addAttribute("title", "Jobs with " +
+                    ListController.columnChoices.get(searchType) + ": " + searchTerm);
+        }
+
+        model.addAttribute("jobs", jobs);
+
+        return "search";
+
+    }
 
 }
